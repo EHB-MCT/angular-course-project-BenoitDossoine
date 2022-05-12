10239e142ed489f381e0f046e6ee74d047adba8b
BenoitDossoine <benoit.dossoine@student.ehb.be>
2022-05-12 12:06:54 +0200

    Adds clickevents to shop tiles

--- steam-redesign/src/app/shop-list/shop-tiles/shop-tile/shop-tile.component.ts
+++ steam-redesign/src/app/shop-list/shop-tiles/shop-tile/shop-tile.component.ts
@@ -7,11 +7,23 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./shop-tile.component.scss']
 })
 export class ShopTileComponent implements OnInit {
+
+  public heartClicked = false;
+  public cartClicked = false;
   @Input() game:any;
   constructor() { }
 
   ngOnInit(): void {
-    
+
+  }
+
+  onHeartClick(){
+    console.log("clicked!")
+    this.heartClicked = !this.heartClicked;
+  }
+
+  onCartClick(){
+    this.cartClicked = !this.cartClicked;
   }
 
 }
