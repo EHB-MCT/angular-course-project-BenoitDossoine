a13b847d447a5cab05aeb0df32780377123c6b73
BenoitDossoine <benoit.dossoine@student.ehb.be>
2022-05-14 12:59:11 +0200

    Details of game are loaded in dynamically

--- steam-redesign/src/app/detail-page/detail-specs/detail-specs.component.ts
+++ steam-redesign/src/app/detail-page/detail-specs/detail-specs.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges } from '@angular/core';
+import { isEmpty } from 'rxjs';
 
 @Component({
   selector: 'app-detail-specs',
@@ -8,9 +9,59 @@ import { Component, Input, OnInit } from '@angular/core';
 export class DetailSpecsComponent implements OnInit {
 
   @Input() game: any;
+  private requirements: any;
+  public recommended: any;
+  public ratings: any;
+
+  public descriptionShown = true;
+  public requirementsShown = false;
+  public reviewsShown = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges():void{
+    this.requirements = this.game.platforms[0].requirements;
+    if(this.requirements.recommended == null){
+      this.recommended = "No requirements found for this game"
+    } else {
+      
+      this.recommended = this.requirements.recommended;
+    }
+
+    this.ratings = this.game.ratings;
+    console.log(this.ratings);
+  }
+
+  showDescription(){
+    document.getElementById("descriptionBtn")?.classList.add('active');
+    document.getElementById("requirementsBtn")?.classList.remove('active');
+    document.getElementById("reviewsBtn")?.classList.remove('active');
+
+    this.descriptionShown = true;
+    this.requirementsShown = false;
+    this.reviewsShown = false;
+  }
+
+  showRequirements(){
+    document.getElementById("descriptionBtn")?.classList.remove('active');
+    document.getElementById("requirementsBtn")?.classList.add('active');
+    document.getElementById("reviewsBtn")?.classList.remove('active');
+
+    this.descriptionShown = false;
+    this.requirementsShown = true;
+    this.reviewsShown = false;
+  }
+  showReviews(){
+    document.getElementById("descriptionBtn")?.classList.remove('active');
+    document.getElementById("requirementsBtn")?.classList.remove('active');
+    document.getElementById("reviewBtn")?.classList.add('active');
+
+    this.descriptionShown = false;
+    this.requirementsShown = false;
+    this.reviewsShown = true;
   }
 
 }
